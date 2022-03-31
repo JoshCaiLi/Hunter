4827a492ab4a540e71cafd62192602f52284464d
jcaili <101600349+jcaili@users.noreply.github.com>
2022-03-31 14:47:00 -0400

    Update 13Imageblues.py

--- 13Imageblues.py
+++ 13Imageblues.py
@@ -2,25 +2,20 @@
 #Email: j.caili@aol.com
 #Date: 3/2/2022
 #This program is easy assignment
-from ensurepip import version
 import matplotlib.pyplot as plt
 import numpy as np
 
-
-img = plt.imread('csBridge.png')   #Read in image from csBridge.png
-plt.imshow(img)		                 #Load image into pyplot
-plt.show()                         #Show the image (waits until closed to continue)
-
+img = plt.imread('csBridge.png')    #Read in image from csBridge.png
 img2 = img.copy()        #make a copy of our image
 img2[:,:,0] = 0         #Set the red channel to 0
-img2[:,:,1] = 0          #Set the gdreen channel to 0         #Set the blue channel to 0
+img2[:,:,1] = 0          #Set the green channel to 0         #Set the blue channel to 0
 
-img3 = img.copy()        #make a copy of our image
+imgin = plt.imread('acTree.png')
+img3 = imgin.copy()        #make a copy of our image
 img3[:,:,0] = 0         #Set the red channel to 0
 img3[:,:,1] = 0          #Set the green channel to 0         #Set the blue channel to 0
 
 
-plt.imshow(img2)         #Load our new image into pyplot
-plt.show()      
-plt.imshow(img3)
-plt.show()               #Show the image (waits until closed to continue)
+plt.imsave('out1.png', img2)  #Save the image we created to the file: reds.png
+
+plt.imsave('out2.png', img3)
