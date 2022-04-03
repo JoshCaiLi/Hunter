291945575940179cd3eaa4da62cd34867fd2f2ac
jcaili <101600349+jcaili@users.noreply.github.com>
2022-04-03 16:58:38 -0400

    Add files via upload

---
+++ 35Afternoons.py
@@ -0,0 +1,13 @@
+#Name:  Your name
+#Date:  March 2022
+#Account name for my github account
+#Modify the program from Lab 7 that displays shelter population over time to:
+
+time = int(input('enter a number: '))
+if (time < 12):
+    print('Good Morning')
+elif (time >= 12 and time<17):
+    print('Good Afternoon')
+else:
+    print('Good Evening')
+    
