d4edbc92183a2972cc97879536a738e050125954
jcaili <101600349+jcaili@users.noreply.github.com>
2022-04-10 23:20:13 -0400

    Add files via upload

--- 36t.py
+++ 36t.py
@@ -1,12 +1,79 @@
 #Name:  Your name
 #Date:  March 2022
 #Account name for my github account
-#Modify the program from Lab 7 that displays shelter population over time to:
-import pandas as pd
+#Modify the program from Lab 7 that displays shelter population over time to
 
-csvFile = input('Enter CSV file name: ')         #Name of the CSV file
-tickets = pd.read_csv(csvFile)  
-attribute = input('enter a attribute: ')
-print("The 10 worst offenders are:")
-print(tickets[attribute].value_counts()[:10]) #Print out the dataframe
 
+def monthString(monthNum):
+   
+     monthString = ""
+     if monthNum == 1:
+        monthString = "January"
+     if monthNum == 2:
+        monthString = "February"
+     if monthNum == 3:
+        monthString = "March"
+     if monthNum == 4:
+        monthString = "April"
+     if monthNum == 5:
+        monthString = "May"
+     if monthNum == 6:
+        monthString = "June"
+     if monthNum == 7:
+        monthString = "July"
+     if monthNum == 8:
+        monthString = "August"
+     if monthNum == 9:
+        monthString = "September"
+     if monthNum == 10:
+        monthString = "October"
+     if monthNum == 11:
+        monthString = "November"
+     if monthNum == 12:
+        monthString = "December"
+    
+
+
+     ###################################
+     ### FILL IN YOUR CODE HERE      ###
+     ### Other than your name above, ###
+     ### this is the only section    ###
+     ### you change in this program. ###
+     ###################################
+
+     return(monthString)
+
+
+
+def main():
+     n = int(input('Enter the number of the month: '))
+     mString = monthString(n)
+     print('The month is', mString)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Allow script to be run directly:
+if __name__ == "__main__":
+     main()
+                   
